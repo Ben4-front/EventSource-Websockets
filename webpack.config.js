6697fe9aca0e8b283f667c7a9da5f5c7bf3d8a2c
Ben4 <benymany511@gmail.com>
2026-01-19 23:49:20 +0300

    Fix paths

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   mode: 'development', 
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '',
+    publicPath: './',
   },
   entry: './src/js/app.js',
   module: {
