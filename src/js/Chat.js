5cb78becda328d4731693906e5af7b9a6687c30d
Ben4 <benymany511@gmail.com>
2026-01-19 17:55:02 +0300

    First commit

--- src/js/Chat.js
+++ src/js/Chat.js
@@ -4,7 +4,7 @@ export default class Chat {
   constructor(container) {
     this.container = container;
 
-    this.baseUrl = 'https://sse-ws-chat-backend.onrender.com';
+    this.baseUrl = 'https://helpdesk-backend-23ri.onrender.com';
     this.api = new ChatAPI(this.baseUrl);
     
     this.user = null;
